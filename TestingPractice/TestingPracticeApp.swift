b66b107a5eed37daf757f15a5f180b1085b14e63
Najia <haiderna@vcu.edu>
2023-09-11 17:15:03 -0400

    Initial Commit

---
+++ TestingPractice/TestingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingPracticeApp.swift
+//  TestingPractice
+//
+//  Created by Najia Haider on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
